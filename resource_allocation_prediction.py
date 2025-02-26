800aa1dc323c36dd1988be4a8b1628c29c9f2af8
Supratim Chakraborty <43934848+supratimrupal@users.noreply.github.com>
2025-02-26 11:12:44 +0100

    Update resource_allocation_prediction.py

--- resource_allocation_prediction.py
+++ resource_allocation_prediction.py
@@ -2,34 +2,34 @@ import streamlit as st
 import numpy as np
 import pickle
 
-# Load the trained model, label encoder, and scaler
-loaded_model = pickle.load(open('saved_model_misc/resource_allocation_optimization_5G.sav', 'rb'))
-label_encoder = pickle.load(open('saved_model_misc/App_type_label_encoder.pkl', 'rb'))
-scaler = pickle.load(open('saved_model_misc/RA_5G_scaler.pkl', 'rb'))
 
-# Streamlit UI
+loaded_model = pickle.load(open('saved_model_misc/resource_allocation_optimization_5G.sav','rb'))
+label_encoder =  pickle.load(open('saved_model_misc/App_type_label_encoder.pkl','rb'))
+scaler =  pickle.load(open('saved_model_misc/RA_5G_scaler.pkl','rb'))
+
+
 st.title("5G Resource Allocation Prediction")
 
-# User Input Fields (without Resource_Allocation)
+
 application_type = st.selectbox("Select Application Type", ['Video_Call', 'Voice_Call', 'Streaming', 'Emergency_Service','Online_Gaming', 'Background_Download', 'Web_Browsing','IoT_Temperature', 'Video_Streaming', 'File_Download', 'VoIP_Call'])
-signal_strength = st.number_input("Enter Signal Strength (dBm)", value=-80)
-latency_in_ms = st.number_input("Enter Latency (ms)", min_value=0, value=30)
-required_bandwidth = st.number_input("Enter Required Bandwidth (Kbps)", min_value=0.001, value=1024.0)
-allocated_bandwidth = st.number_input("Enter Allocated Bandwidth (Kbps)", min_value=0.001, value=1024.0)
+signal_strength = st.number_input ("Enter Signal Strength (dBm)" , value=-80)
+latency = st.number_input ("Enter Latency (ms)" , min_value=0, value=30)
+required_bandwidth = st.number_input("Enter Required Bandwidth (Kbps)" ,min_value=0.001, value=1024.0)
+allocated_bandwidth = st.number_input("Enter Allocated Bandwidth (Kbps)", min_value=0.001 , value=1024.0)
 
 
 bandwidth_utilization = allocated_bandwidth / required_bandwidth 
 
 
-application_type_encoded = label_encoder.transform([application_type])[0]
+application_type = label_encoder.transform([application_type])[0]
 
 
-input_data_processed = np.array([[application_type_encoded, signal_strength, latency_in_ms, bandwidth_utilization]])
+input_5G_data = np.array([[application_type, signal_strength, latency ,bandwidth_utilization]])
 
 
-input_data_scaled = scaler.transform(input_data_processed)
+input_5G_data_scaled = scaler.transform(input_5G_data)
 
 
 if st.button("Predict Resource Allocation"):
-    prediction = loaded_model.predict(input_data_scaled)
-    st.success(f"Predicted Resource Allocation: {prediction[0]:.2f}%")
+    prediction = loaded_model.predict(input_5G_data_scaled)
+    st.success("Predicted Resource Allocation: " + str(round(prediction[0], 2)) + "%")
